3a89ca9c3b1ef72302af8a06862d502f66aeb9d8
adam <redndahead@gmail.com>
2013-09-23 15:12:40 -0700

    Missing part of the action url.

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,4 +1,4 @@
-  <form action="http://stanford.us5.list-manage2.com/subscribe?u=ce785d9b9016cd" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
+  <form action="http://stanford.us5.list-manage2.com/subscribe?u=ce785d9b9016cd35fb68e83b7&id=0b5214e34b" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
     <label for="edit-email" class="footer__signup__label"><a href="/contact/rethink">Sign Up for Our eNews</a></label>
     <p class="footer__signup__description">Get the latest business ideas and insights from <em>Stanford Business Re:Think</em></p>
     <input type="text" id="edit-email" placeholder="email address" name="MERGE0" value="" size="60" maxlength="128" class="form-text footer__signup__email">
