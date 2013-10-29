54b36754a00a35a62590d3f175d8e2459a1b82b7
adam <redndahead@gmail.com>
2013-10-29 15:57:56 -0700

    WP-2267: Fix javascript on mobile program finder.

--- js/gsb_public_custom_blocks.js
+++ js/gsb_public_custom_blocks.js
@@ -1,35 +1,41 @@
+
 (function ($) {
   Drupal.behaviors.gsbPublicCustomBlocks = {
       attach: function () {
-        $('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a').hover(function(){
-          $(this).click();
-        });
-        $('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a').click(function(){
-          window.location.hash = $(this).find('strong').text();
-        });
+        // Update link to be the right anchor.
         $('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a strong').each(function (index) {
           var text = $(this).text();
-          $(this).parent('a').attr('href', '#' + text);
-          if (window.location.hash && !Drupal.settings.gsb_public_custom_blocks) {
-            //open tab if anchor present
-            var anchor = window.location.hash;
-            anchor = anchor.replace('%20', ' ');
-            if (anchor === '#' + text) {
-              $(this).parent('a').click();
-              $('div.pane-program-finder').attr('id', 'pane-program-finder');
-              window.location.hash = 'pane-program-finder';
-              window.location.hash = text;
-            }
-          }
+          $(this).parent('a').attr('href', '#' + text).attr('name', text);
         });
-        Drupal.settings.gsb_public_custom_blocks = {};
-        Drupal.settings.gsb_public_custom_blocks.activated = true;
 
         // Add our responsive javascript.
         if (Modernizr.mq('(max-width: 1165px)')) {
           $('.pane-program-finder .vertical-tabs-list').addClass('resp-tabs-list');
-          $('.pane-program-finder .vertical-tabs').easyResponsiveTabs({type: 'accordion'});
+          $('.pane-program-finder .vertical-tabs').easyResponsiveTabs({type: 'accordion', activate: Drupal.gsbPublicCustomBlocks.clicked});
+
+          // Activate tab if we have an anchor.
+          $('div.pane-program-finder li.vertical-tab-button a[name="' + window.location.hash.substring(1) + '"]').parent().click();
+        }
+        else {
+          // Load the tab on hover.
+          $('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a').hover(function(){
+            $(this).click();
+          });
+
+          // Activate tab if we have an anchor.
+          $('div.pane-program-finder li.vertical-tab-button a[name="' + window.location.hash.substring(1) + '"]').click();
         }
       }
   };
+
+  Drupal.gsbPublicCustomBlocks = Drupal.gsbPublicCustomBlocks || {};
+
+  // We need to scroll the window into view if a tab has been clicked in mobile
+  Drupal.gsbPublicCustomBlocks.clicked = function(currentTab) {
+    // Make sure the animation is completed before we scroll it into view.
+    $(currentTab.target).parent().find('.resp-tab-content-active').promise().done(function() {
+      $(currentTab.target)[0].scrollIntoView();
+    });
+  }
 })(jQuery);
+
