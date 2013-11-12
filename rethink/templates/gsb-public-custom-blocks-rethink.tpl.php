4d5fc3bf194210a4ceed2ecfea653b72e4ddea8c
hgupta <hgupta@ciber.com>
2013-11-12 14:25:12 -0800

    WP-2330: Updated the title and text for rethink block

--- rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
+++ rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
@@ -1,8 +1,8 @@
 <a href="/contact/rethink" class="designed-box cta-rethink">
   <p class="cta-title">
-    <b>RE:</b>THINK
+    Stanford Business <b>RE:</b>THINK
   </p>
   <p class="cta-description">
-    Get the latest business ideas and insights.<b> Sign up for our eNews</b><i></i>
+    Your source for management ideas and insights, delivered to your inbox.<i></i>
   </p>
 </a>
