4c68f260748b58819a4227634b1ce9c5417a0b17
hgupta <hgupta@ciber.com>
2013-11-13 15:08:23 -0800

    WP-2330:updated the Enews block and revert back the changes for Rethink block

--- rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
+++ rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
@@ -1,8 +1,8 @@
 <a href="/contact/rethink" class="designed-box cta-rethink">
   <p class="cta-title">
-    Stanford Business <b>RE:</b>THINK
+    <b>RE:</b>THINK
   </p>
   <p class="cta-description">
-    Your source for management ideas and insights, delivered to your inbox.<i></i>
+    Get the latest business ideas and insights.<b> Sign up for our eNews</b><i></i>
   </p>
 </a>
