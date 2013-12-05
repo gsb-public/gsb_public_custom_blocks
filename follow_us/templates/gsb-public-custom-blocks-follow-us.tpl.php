c2a38d32ef3582144b72a12e6e177e3c82e6baa8
Meghan Palagyi <meghanp@stanford.edu>
2013-12-04 16:02:50 -0800

    WP-2472: update icons and twitter feed.

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -1,9 +1,7 @@
-<div class="designed-box cta-follow-us">
-  <a class="cta-title" href="/contact/social">Follow Us<i></i></a>
-  <div class="icon-wrapper">
-    <a href="https://twitter.com/stanfordbiz" target="_blank" class="circle social-block-twitter"></a>
-    <a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a>
-    <a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a>
-    <a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a>
-  </div>
-</div>
+<h2><a class="social-title" href="/contact/social">Follow Us</a></h2>
+<ul class="icon-wrapper">
+  <li><a href="https://twitter.com/stanfordbiz" target="_blank" class="circle social-block-twitter"></a></li>
+  <li><a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a></li>
+  <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a></li>
+  <li><a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a></li>
+</ul>
