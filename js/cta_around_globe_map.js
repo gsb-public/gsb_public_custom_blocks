7d60ff47d3ef5d7bad47897b8863f57c799ae4be
Adam Moore <redndahead@gmail.com>
2014-02-25 07:52:17 -0800

    Add new id to around the globe map.

--- js/cta_around_globe_map.js
+++ js/cta_around_globe_map.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.ctaAroundGlobeMap = {
     attach: function () {
     	try {
-    		var map = L.mapbox.map("map", "jennyhutch.gsb-main-data");
+    		var map = L.mapbox.map("map", "jennyhutch.h1g0laoo");
 	    	map.gridControl.options.sanitizer = function(x) { return x; };    	
     	} catch(e) {
     	}
