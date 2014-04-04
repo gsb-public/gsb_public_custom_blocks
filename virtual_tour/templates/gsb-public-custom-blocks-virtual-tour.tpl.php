eb5e09a9f117a9aa1d1b31987f98e95e1d7bb0b7
Meghan Palagyi <meghanp@stanford.edu>
2014-04-04 14:50:36 -0700

    WP-3336: remove empty italic tag.

--- virtual_tour/templates/gsb-public-custom-blocks-virtual-tour.tpl.php
+++ virtual_tour/templates/gsb-public-custom-blocks-virtual-tour.tpl.php
@@ -1,4 +1,4 @@
 <a href="/visit/closer-look-campus" class="designed-box cta-virtual-tour">
-  <span class="virtual-tour-btn">Take a Closer Look<i></i></span>
+  <span class="virtual-tour-btn">Take a Closer Look</span>
   <div class="virtual-tour-cover"></div>
 </a>
