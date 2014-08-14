078c71898eb61a372753b0cb519ae79e8655bfc0
Greg Mercer (GBear) <gmercer@stanford.edu>
2014-08-14 11:09:43 -0700

    WP-4170 New insights search block

---
+++ insights_search/templates/gsb-public-custom-blocks-insights-search.tpl.php
@@ -0,0 +1,7 @@
+<div class="form-item form-type-textfield form-container-insights-search">
+  <form class="grey-search-box" action="/insights" method="post" id="insights-search-block-form" accept-charset="UTF-8">
+    <label class="element-invisible" for="edit-insights-search-keys">Enter the terms you wish to search for. </label>
+    <input placeholder="search" type="text" id="edit-insights-search-keys" name="search_keys" value="" size="15" maxlength="128" class="form-text">
+    <input type="submit" id="edit-submit-insights-search" name="op" value="Search" class="form-submit">
+  </form>  
+</div>
