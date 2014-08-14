50af614e76a6ed5ee2191ee979c457869b30f47d
Greg Mercer (GBear) <gmercer@stanford.edu>
2014-08-14 11:18:04 -0700

    WP-4170 A little cleanup of tabs to spaces

--- insights_search/js/insights_search.js
+++ insights_search/js/insights_search.js
@@ -2,20 +2,19 @@
   Drupal.behaviors.gsb_public_custom_blocks_insights_search = {
     attach: function (context, settings) {
       $('#edit-submit-insights-search').click(function() {
-     		// Don't submit the form just yet.
-      	event.preventDefault();
-      	if (!$('#edit-insights-search-keys').is(":visible")) {
-      		$('#edit-insights-search-keys').show();
-				  $('#edit-insights-search-keys').animate({
-				    //width: [ "toggle", "swing" ]
-				    width: '75%'
-				  }, 500, 'linear', function() {
-				    // done
-				  });      		
-      	}
-      	else {
-      		// todo: add submit handling here
-      	}
+        // Don't submit the form just yet.
+        event.preventDefault();
+        if (!$('#edit-insights-search-keys').is(":visible")) {
+          $('#edit-insights-search-keys').show();
+          $('#edit-insights-search-keys').animate({
+            width: '75%'
+          }, 500, 'linear', function() {
+            // done
+          });
+        }
+        else {
+          // todo: add submit handling here
+        }
       });
     }
   };
