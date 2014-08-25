22c727206f1774abd62c42622e9bc4527e35c3a8
Meghan Palagyi <meghanp@stanford.edu>
2014-08-25 09:31:49 -0700

    WP-4141: updat search markup.

--- insights_search/templates/gsb-public-custom-blocks-insights-search.tpl.php
+++ insights_search/templates/gsb-public-custom-blocks-insights-search.tpl.php
@@ -1,7 +1,8 @@
 <div class="form-item form-type-textfield form-container-insights-search">
+  <div id="insights-search-touch-button"><span class="element-invisible">Search the Insights section</span></div>
   <form class="grey-search-box" action="/insights" method="post" id="insights-search-block-form" accept-charset="UTF-8">
     <label class="element-invisible" for="edit-insights-search-keys">Enter the terms you wish to search for. </label>
-    <input placeholder="search" type="text" id="edit-insights-search-keys" name="search_keys" value="" size="15" maxlength="128" class="form-text">
+    <input placeholder="search Insights" type="text" id="edit-insights-search-keys" name="search_keys" value="" size="15" maxlength="128" class="form-text">
     <input type="submit" id="edit-submit-insights-search" name="op" value="Search" class="form-submit">
-  </form>  
+  </form>
 </div>
