e8cb94fca37ac169acf610dfc594ff2b7525f77b
Roopesh Naik <rnaik77@stanford.edu>
2014-09-18 22:55:39 +0530

    WP-4393 adjust the form search field

--- insights_search/templates/gsb-public-custom-blocks-insights-search.tpl.php
+++ insights_search/templates/gsb-public-custom-blocks-insights-search.tpl.php
@@ -2,7 +2,7 @@
   <div id="insights-search-touch-button"><span class="element-invisible">Search the Insights section</span></div>
   <form class="grey-search-box" action="/insights/search" method="get" id="insights-search-block-form" accept-charset="UTF-8">
     <label class="element-invisible" for="edit-insights-search-keys">Enter the terms you wish to search for. </label>
-    <input placeholder="search Insights" type="text" id="edit-insights-search-keys" name="search_keys" value="" size="15" maxlength="128" class="form-text">
+    <input placeholder="search Insights" type="text" id="edit-insights-search-keys" name="term" value="" size="15" maxlength="128" class="form-text">
     <input type="submit" id="edit-submit-insights-search" name="op" value="Search" class="form-submit">
   </form>
 </div>
