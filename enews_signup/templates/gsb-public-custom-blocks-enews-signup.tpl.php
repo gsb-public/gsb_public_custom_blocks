905121be6855045eeef9f422aa981d290d047484
Greg Mercer (GBear) <gmercer@stanford.edu>
2014-09-26 14:32:35 -0700

    WP-4442 Changed text from ReThink to Email

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,5 +1,5 @@
   <form action="http://stanford.us5.list-manage2.com/subscribe?u=ce785d9b9016cd35fb68e83b7&id=0b5214e34b" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
-    <label for="edit-email" class="footer__signup__label"><a href="/contact/rethink">Stanford Business Re:Think</a></label>
+    <label for="edit-email" class="footer__signup__label"><a href="/contact/rethink">Stanford Business Email</a></label>
     <p class="footer__signup__description">Your source for management ideas and insights, delivered to your inbox.</p>
     <input type="text" id="edit-email" placeholder="email address" name="MERGE0" value="" size="60" maxlength="128" class="form-text footer__signup__email">
     <input type="submit" id="edit-submit" name="op" value="GO" class="form-submit footer__signup__submit">
