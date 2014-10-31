40bb3778a67c8e52d5970b1550772af981ff1889
Roopesh Naik <rnaik77@stanford.edu>
2014-10-31 16:12:52 +0530

    WP-4589 removed CTA GSB around the Globe and World

--- around_globe/templates/gsb-public-custom-blocks-around-globe.tpl.php
+++
@@ -1,7 +0,0 @@
-<div class="designed-box cta-around-globe">
-  <a href="/stanford-gsb-experience/gsb-around-world" class="around-globe-link">
-    <div class="link-inner">
-      <div class="link-title">GSB Around <br /> the World</div>
-    </div>
-  </a>
-</div>
