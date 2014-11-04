0867bef4f4924df0bfcefd385202f30a0effd085
Roopesh Naik <rnaik77@stanford.edu>
2014-11-04 10:48:21 +0530

    WP-4589 remove unused items

--- about_campus/templates/gsb-public-custom-blocks-about-campus.tpl.php
+++
@@ -1,4 +0,0 @@
-<a href="/stanford-gsb-experience/life-stanford-gsb/campus" class="learn-about-campus">
-  <span class="about-campus-btn">Learn About<br /> Our Campus</span>
-  <div class="about-campus-cover"></div>
-</a>
