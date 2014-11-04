27d7994308bd4b68cf75afbef393cb37def42069
Roopesh Naik <rnaik77@stanford.edu>
2014-11-04 10:48:21 +0530

    WP-4589 remove unused items

--- rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
+++
@@ -1,5 +0,0 @@
-<h1 class="rethink-logo">Re:Think</h1>
-<div class="rethink-wrapper">
-  <div class="rethink-description">Your source for business ideas and insights. Delivered to your inbox.</div>
-  <a href="/contact/rethink" class="rethink-link">Subscribe</a>
-</div>
