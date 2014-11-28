ede6b3eb95980c2672aa3d78c06cf1ecd090bd14
Roopesh Naik <rnaik77@stanford.edu>
2014-11-28 10:53:31 +0530

    WP-4736 delete items from modal window

--- virtual_tour/templates/gsb-public-custom-blocks-virtual-tour.tpl.php
+++
@@ -1,4 +0,0 @@
-<a href="/visit/closer-look-campus" class="designed-box cta-virtual-tour">
-  <span class="virtual-tour-btn">Take a Closer Look</span>
-  <div class="virtual-tour-cover"></div>
-</a>
