e3050fe4bb3ce3b97e42e56ea4ac29492225733f
Ayuta Padhi <apadhi@stanford.edu>
2016-03-03 20:37:05 -0800

    WP-6698 Added stay informed custom block

---
+++ exec_ed/templates/gsb-public-custom-blocks-stay_informed.tpl.php
@@ -0,0 +1,12 @@
+<div class="content">
+  <form action="http://www.gsb.stanford.edu/exec-ed/contact/subscribe" method="get" id="gsb-stay-informed-signup-form" accept-charset="UTF-8" class="footer-signup">
+    <label for="edit-email" class="signup-label">Stay Informed</label>
+    <p class="signup-description">Sign up for our Mailing list to receive the latest news and program offerings.</p>
+    <div class="signup-wrapper">
+      <div class="signup-email-wrapper">
+        <input type="text" id="edit-submitted-email-address" placeholder="email address" name="email_address" value="" size="60" maxlength="128" class="form-text signup-email">
+      </div>
+      <input type="submit" id="edit-submit" name="op" value="Go" class="form-submit signup-submit">
+    </div>
+  </form>
+</div>
