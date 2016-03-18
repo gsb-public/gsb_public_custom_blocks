38a9070624d0660c180eddfa1ce844ea35faa240
Ayuta Padhi <apadhi@stanford.edu>
2016-03-18 10:46:31 -0700

    WP-6785 Added seed stay informed custom block

---
+++ seed/templates/gsb-public-custom-blocks-seed_stay_informed.tpl.php
@@ -0,0 +1,12 @@
+<div class="content">
+  <form action="http://www.gsb.stanford.edu/seed/contact/stay-informed" method="get" id="gsb-seed-stay-informed-signup-form" accept-charset="UTF-8" >
+    <label for="edit-email" class="signup-label">Stay Informed</label>
+    <p class="signup-description">Sign up for our Mailing list to receive the latest news and program offerings.</p>
+    <div class="signup-wrapper">
+      <div class="signup-email-wrapper">
+        <input type="text" id="edit-submitted-email-address" placeholder="email address" name="email_address" value="" size="60" maxlength="128" class="form-text signup-email">
+      </div>
+      <input type="submit" id="edit-submit" name="op" value="Go" class="form-submit signup-submit">
+    </div>
+  </form>
+</div>
