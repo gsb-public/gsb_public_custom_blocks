650359eaab852a547c9c8e4c93ef1f6fe96a4688
Ayuta Padhi <apadhi@stanford.edu>
2016-04-10 19:27:04 -0700

    WP-6732 Implementation of mini program finder

---
+++ exec_ed/templates/gsb-public-custom-blocks-mini_program_finder.tpl.php
@@ -0,0 +1,11 @@
+<div >
+  <div class="finder-image" >
+    <?php print theme('image_style',array('style_name' => '816x_variable', 'path' => $finder_img->uri));?>
+    <div class="apply-mini-finder">
+      <?php print l(t("Apply"), '/exec-ed/admission'); ?>
+    </div>
+   </div>
+</div>
+<div class="content">
+  <h4 ><?php print t('Find Your Program'); ?></h4>
+</div>
