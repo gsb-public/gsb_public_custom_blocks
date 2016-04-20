805eb22077bdbf44cbc0a618d547861f58f5cd22
Ayuta Padhi <apadhi@stanford.edu>
2016-04-20 15:28:48 -0700

    WP-7011 Added GSB Full-time degree program custom block

---
+++ degree_programs/templates/gsb-public-custom-blocks-degree_programs.tpl.php
@@ -0,0 +1,16 @@
+<div class="degree-programs-wrapper">
+  <?php
+  if (!empty($degree_programs)): ?>
+    <?php foreach ($degree_programs as $it => $itd): ?>
+      <div class="degree-program">
+        <div class="title">
+          <?php print l(t($itd['title']), drupal_lookup_path('alias', "node/". $it)); ?>
+          <?php //print $itd['title']; ?>
+        </div>
+        <div class="intro">
+          <?php print $itd['program_teaser']; ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  <?php endif; ?>
+</div>
