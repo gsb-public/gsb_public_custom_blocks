716431175c77506090fbd0c52f88076e26c6a195
Meghan Plunkett <meghanp@stanford.edu>
2016-07-07 15:25:05 -0400

    WP-7409: rearrage button position.

--- exec_ed/templates/gsb-public-custom-blocks-mini_program_finder.tpl.php
+++ exec_ed/templates/gsb-public-custom-blocks-mini_program_finder.tpl.php
@@ -1,6 +1,6 @@
-<div class="mini-program-finder-image" >
-  <?php print theme('image_style',array('style_name' => '816x_variable', 'path' => $finder_img->uri));?>
-</div>
 <div class="mini-program-finder-apply">
   <?php print l(t("Apply"), '/exec-ed/admission'); ?>
+</div>
+<div class="mini-program-finder-image" >
+  <?php print theme('image_style',array('style_name' => '816x_variable', 'path' => $finder_img->uri));?>
 </div>
