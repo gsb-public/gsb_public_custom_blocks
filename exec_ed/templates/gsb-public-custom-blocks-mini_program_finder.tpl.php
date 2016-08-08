dbcc1dbbb6519f704cf5275965b85caf793f2579
Ayuta Padhi <apadhi@stanford.edu>
2016-08-08 11:04:52 -0700

    WP-7574 swiched image style to tinypng compression

--- exec_ed/templates/gsb-public-custom-blocks-mini_program_finder.tpl.php
+++ exec_ed/templates/gsb-public-custom-blocks-mini_program_finder.tpl.php
@@ -2,5 +2,5 @@
   <?php print l(t("Apply"), '/exec-ed/admission'); ?>
 </div>
 <div class="mini-program-finder-image" >
-  <?php print theme('image_style',array('style_name' => '816x_variable', 'path' => $finder_img->uri));?>
+  <?php print theme('image_style',array('style_name' => 'program_display_image', 'path' => $finder_img->uri));?>
 </div>
