dc2af711ded4e0fdfdbd95f782bf25356dc4672c
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-09-16 07:37:24 -0700

    WP-7632 Moved the gsb_tweetfeed.init() call into a .js file - which gets attached to the js in the footer

---
+++ js/homepage_social_feed.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+ /**
+  * Behaviour for HomePage Social Feed block.
+  */
+ Drupal.behaviors.homepage_social_feed = {
+   attach: function (context, settings) {
+    gsb_tweetfeed.init({
+      search: '@stanfordbiz',
+      numTweets: 2,
+      appendTo: '#fpp-tweets-front',
+      format: 'l, M j | a'
+    });
+  }
+ }
+
+})(jQuery);
