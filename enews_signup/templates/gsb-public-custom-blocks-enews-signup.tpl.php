69fbf22efe1cc8ec982e3dd556a4642179193f03
Ayuta Padhi <apadhi@stanford.edu>
2016-12-09 10:32:28 -0800

    WP-7722 updated url to use https for enews-signup block

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,4 +1,4 @@
-  <form action="http://stanford.us5.list-manage2.com/subscribe?u=ce785d9b9016cd35fb68e83b7&id=0b5214e34b" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer-signup">
+  <form action="https://stanford.us5.list-manage.com/subscribe?u=ce785d9b9016cd35fb68e83b7&id=0b5214e34b" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer-signup">
     <label for="edit-email" class="signup-label"><a href="/contact/rethink">Stanford Business Email</a></label>
     <p class="signup-description">Your source for management ideas and insights, delivered to your inbox.</p>
     <div class="signup-wrapper">
