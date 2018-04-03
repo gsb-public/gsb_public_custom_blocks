291ae1d726bd1428cb714508af53dc5665ec9b04
Roopesh Naik <rnaik77@stanford.edu>
2018-04-03 19:29:23 +0530

    WP-8246 add custom block for stay in touch

---
+++ stay_in_touch/templates/gsb-public-custom-blocks-stay-in-touch.tpl.php
@@ -0,0 +1,6 @@
+<div class="parallax">
+  <div class="stay-in-touch">
+    <h2>Stay in Touch</h2>
+    <a href="contact/subscriptions" class="btn btn-default btn-lg">Sign Up for Stanford GSB Newsletters</a>
+  </div>
+</div>
