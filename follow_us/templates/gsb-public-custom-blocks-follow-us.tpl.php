bee850777274b79247fcbf19a6bc18eb14c6bda9
Vijeesh Vijayan <vvijayan@CIBER-INDIA.IN>
2018-07-30 18:06:51 +0530

    WP-8487 social medai icon order changed

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -1,8 +1,7 @@
 <h2><a class="social-title" href="/insights/follow">Follow Us</a></h2>
 <ul class="icon-wrapper">
   <li><a href="https://twitter.com/stanfordgsb" target="_blank" class="circle social-block-twitter"></a></li>
-  <li><a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a></li>
-   <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a></li>
   <li><a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a></li>
- <!-- <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a></li>-->
+  <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a></li>
+  <li><a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a></li>
 </ul>
