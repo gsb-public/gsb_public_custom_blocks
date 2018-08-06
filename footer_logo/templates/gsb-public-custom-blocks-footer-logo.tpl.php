933eb7975d1de6aa2f572074c41d3774df4fe1a1
Vijeesh Vijayan <vvijayan@CIBER-INDIA.IN>
2018-08-06 12:42:48 +0530

    WP-8492 footer logo block for new footer

---
+++ footer_logo/templates/gsb-public-custom-blocks-footer-logo.tpl.php
@@ -0,0 +1,5 @@
+<div id="footer-logo" class="">
+<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="ftr-logo">
+<img src="/<?php print drupal_get_path('theme', 'gsb_theme'); ?>/images/logo-print.jpg" alt="Home">
+</a>
+</div>
