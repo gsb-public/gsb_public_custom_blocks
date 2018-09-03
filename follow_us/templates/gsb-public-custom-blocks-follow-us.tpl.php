c2d5aca9416eee2b810677cf9ca6bda9fb8bc94c
Vijeesh Vijayan <vvijayan@CIBER-INDIA.IN>
2018-09-03 19:33:02 +0530

    WP-8536 added css class for follow us template

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -2,7 +2,7 @@
 <ul class="icon-wrapper">
   <li><a href="https://twitter.com/stanfordgsb" target="_blank" class="circle social-block-twitter"></a></li>
   <li><a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a></li>
-  <li><a href="https://www.instagram.com/stanfordgsb" target="_blank" class="social-block-insta"></a></li>
+  <li class="social-insta-block"><a href="https://www.instagram.com/stanfordgsb" target="_blank" class="social-block-insta"></a></li>
   <li><a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a></li>
   <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a></li>
 </ul>
