631c08ca6c5f8bcf8b919f6c47907209ee3d2c81
JuanGongora <gongora.animations@gmail.com>
2019-02-27 14:48:34 -0500

    WP-8887 changed url in template

--- stay_in_touch/templates/gsb-public-custom-blocks-stay-in-touch.tpl.php
+++ stay_in_touch/templates/gsb-public-custom-blocks-stay-in-touch.tpl.php
@@ -1,6 +1,6 @@
 <div class="parallax">
   <div class="stay-in-touch">
     <h2>Stay in Touch</h2>
-    <a href="contact/subscriptions" class="cta">Sign Up for Stanford GSB Newsletters</a>
+    <a href="contact/stay-touch" class="cta">Sign Up for Stanford GSB Newsletters</a>
   </div>
 </div>
