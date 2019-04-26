9d496896a2691788e9e826e2ca91e30a07213a94
JuanGongora <gongora.animations@gmail.com>
2019-04-26 13:54:44 -0400

    WP-9077 changed CTA name string in block

--- stay_in_touch/templates/gsb-public-custom-blocks-stay-in-touch.tpl.php
+++ stay_in_touch/templates/gsb-public-custom-blocks-stay-in-touch.tpl.php
@@ -1,6 +1,6 @@
 <div class="parallax">
   <div class="stay-in-touch">
     <h2>Stay in Touch</h2>
-    <a href="contact/stay-touch" class="cta">Sign Up for Stanford GSB Newsletters</a>
+    <a href="contact/stay-touch" class="cta">Sign Up for Stanford GSB Updates</a>
   </div>
 </div>
