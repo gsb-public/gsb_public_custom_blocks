d58aeb38784fc9e6d246f69db98325151888d9e1
vijeeshciber <VVijayan@ciber.com>
2019-08-21 17:50:18 +0530

    WP-9207 print functionality related changes

--- js/impact_compass_webform.js
+++ js/impact_compass_webform.js
@@ -2,10 +2,63 @@
 
 Drupal.behaviors.impact_compass_webform = {
   attach: function (context, settings) {
+  $("#errorMessage").hide();
+  setTimeout(function() {
+   let company = $("#company").val();
+   let society = $("#valueToSocietyScore").val();
+   let efficacy = $("#efficacyScore").val();
+   let impactMagnitude = $("#impactMagnitudeScore").val();
+   let scalability = $("#scalabilityScore").val();
+   let missionAlignment = $("#missionAlignmentScore").val();
+   let esg = $("#esgScore").val();
+     var ctx = document.getElementById("myChart").getContext("2d");
+     Chart.defaults.global.defaultFontSize = "20";
+     Chart.defaults.global.defaultFontFamily = "NewsGothicBT","Avant Garde","Futura","Helvetica Neue","Helvetica,Arial,sans-serif";
+      var myChart = new Chart(ctx, {
+          type: "radar",
+          data: {
+              labels: ["Value to Society", "Efficacy", "Impact Magnitude", "Scalability", "Mission Alignment", "ESG"],
+              datasets: [{
+                  backgroundColor: "rgba(93,132,159, 0.2)",
+                  borderColor: "rgb(93,132,159)",
+                  pointBackgroundColor: "rgba(0,0,0,0.5)",
+                  pointHoverBorderColor: "rgba(255,0,0,1)",
+                  pointHoverBackgroundColor: "rgba(255,0,0,0.5)",
+                  data: [],
+          }]
+      },
 
-     $("#errorMessage").hide();
-      // A function to get the url parameter.
-    $.urlParam = function(name){
+    options: {
+    legend: {
+          display: false,
+        position: "bottom",
+        fontSize: 12
+          },
+          title: {
+              display: false,
+              text: "Impact Compass"
+          },
+          scale: {
+              pointLabels: {
+                  fontSize: 14,
+              },
+
+        ticks: {
+            min: 0, 
+            max: 3, 
+          fixedStepSize: 1,
+          beginAtZero: true, 
+          fontSize: 12,
+              }
+          }
+      }
+
+    });
+
+  }, 2000);
+
+  // A function to get the url parameter.
+  $.urlParam = function(name){
       var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
       if (results==null){
         return null;
@@ -16,7 +69,6 @@ Drupal.behaviors.impact_compass_webform = {
     }
 
     //Check if the values have been passed through the url.
-
     // Get the values from the url.
     let company = $.urlParam('company');
     let society = $.urlParam('society');
@@ -54,14 +106,21 @@ Drupal.behaviors.impact_compass_webform = {
       renderChart();
     });
 
+    /**
+     * Print pdf functionality
+     */
     $('#print_bt').click(function (e) {
        e.preventDefault();
+       $("#button-block").hide();
+       //$("#print_bt").hide();
+       $(".chart-container").css({"width": "95%", "height": "60%"});
+       $(".chart-score").css({"text-align": "left","padding-left": "100px"});
        $("#imapct-compass-container").printThis({
             debug: false,               // show the iframe for debugging
             importCSS: true,            // import parent page css
             importStyle: true,         // import style tags
             printContainer: true,       // print outer container/$.selector
-            loadCSS: "",                // path to additional css file - use an array [] for multiple
+            loadCSS: "", // path to additional css file - use an array [] for multiple
             pageTitle: "",              // add title to print page
             removeInline: false,        // remove inline styles from print elements
             removeInlineSelector: "*",  // custom selectors to filter inline styles. removeInline must be true
@@ -71,13 +130,18 @@ Drupal.behaviors.impact_compass_webform = {
             base: false,                // preserve the BASE tag or accept a string for the URL
             formValues: true,           // preserve input/form values
             canvas: true,              // copy canvas content
-            doctypeString: '...',       // enter a different doctype for older markup
+            doctypeString: '',       // enter a different doctype for older markup
             removeScripts: false,       // remove script tags from print content
             copyTagClasses: false,      // copy classes from the html & body tag
             beforePrintEvent: null,     // function for printEvent in iframe
             beforePrint: null,          // function called before iframe is filled
             afterPrint: null            // function called before iframe is removed
         });
+       setTimeout(function() {
+        $("#button-block").show();
+        $(".chart-score").css({"text-align": "center","padding-left": "0px"});
+       }, 2000);
+
     });
 
     /**
@@ -95,7 +159,7 @@ Drupal.behaviors.impact_compass_webform = {
       }
 
       let society = $("select[name='valueToSocietyScore']").val();
-      console.log(society);
+      //console.log(society);
       if (society == '') {
         console.log(society);
         $errors.append('Value to Society is required.<br />');
@@ -125,17 +189,15 @@ Drupal.behaviors.impact_compass_webform = {
       if (esg == '') {
         $errors.append('ESG is required.<br />');
       }
-
       if ($errors.text()) {
-        jQuery('html, body').animate({
+        /*jQuery('html, body').animate({
             scrollTop: 0
-        }, 'slow');
+        }, 'slow');*/
         $("#errorMessage").show();
         return;
       }
       $("#errorMessage").hide();
       // Calculate the impact score.
-
       // Render the chart.
       let ctx = document.getElementById("myChart").getContext("2d");
       Chart.defaults.global.defaultFontSize = '20';
@@ -180,10 +242,10 @@ Drupal.behaviors.impact_compass_webform = {
         }
 
       });
+      //myChart.update();
 
       // Set the impact score
       let impactScore = parseInt(society) * parseInt(efficacy) * parseInt(impactMagnitude) * parseInt(scalability) * parseInt(missionAlignment) * parseInt(esg);
-      //alert(society );
       $('#impactText').text(company + ' has an Impact Potential Score of: ' );
       $('#impactScore').text(' '+impactScore);
       // Push the new url parameters to the page.
