6e93397f020c729651eeca49d364c2e9c67e01be
Jennifer Breese Kauth <jbreese@stanford.edu>
2019-10-24 10:56:12 -0700

    WP-9252 adding text to the footer follow us link

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -1,8 +1,8 @@
 <h2><a class="social-title" href="/insights/follow">Follow Us</a></h2>
 <ul class="icon-wrapper">
-  <li><a href="https://twitter.com/stanfordgsb" target="_blank" class="circle social-block-twitter"></a></li>
-  <li><a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a></li>
-  <li class="social-insta-block"><a href="https://www.instagram.com/stanfordgsb" target="_blank" class="social-block-insta"></a></li>
-  <li><a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a></li>
-  <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a></li>
+  <li><a href="https://twitter.com/stanfordgsb" target="_blank" class="circle social-block-twitter"><span>Twitter</span></a></li>
+  <li><a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"><span>Facebook</span></a></li>
+  <li class="social-insta-block"><a href="https://www.instagram.com/stanfordgsb" target="_blank" class="social-block-insta"><span>Instagram</span></a></li>
+  <li><a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"><span>YouTube</span></a></li>
+  <li><a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"><span>LinkedIn</span></a></li>
 </ul>
